29f2ea83cb2e4ecd30fa24ac3bc0d8045c4e6484
littleflute <13699175164@sina.cn>
2017-07-26 14:59:19 -0500

    Add files via upload

---
+++ Johan de Meij/Johan de Meij- Symphony No. 1 'Lord of the Rings'/m.js
@@ -0,0 +1,8 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 1. Gandalf (The Wizard).mp3";		
+f[2]	= "02 2. Lothl¨®rien (The Elvenwood).mp3";		
+f[3]	= "03 3. Gollum (Sm¨¦agol).mp3";		
+f[4]	= "04 4. Journey in the Dark- A. The Mines of Moria; B. The Bridge of Khazad-.mp3";		
+f[5]	= "05 5. Hobbits.mp3";		
+f[6]	= "06 The Sorcerer's Apprentice (L'apprenti sorcier), symphonic scherzo for orche.mp3";		
