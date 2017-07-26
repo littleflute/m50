8b7bb759aa8e37c691a580a747ce80cf404b49aa
littleflute <13699175164@sina.cn>
2017-07-26 15:02:11 -0500

    Add files via upload

---
+++ Lei Liang/LUMINOUS/m.js
@@ -0,0 +1,7 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Verge Quartet.mp3";		
+f[2]	= "03 The moon is following us.mp3";		
+f[3]	= "Inkscape.mp3";		
+f[4]	= "Luminous.mp3";		
+f[5]	= "Trans.mp3";		
